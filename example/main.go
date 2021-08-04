694d1cfaf39f6f1b41a79c4cc3dbf87d09b26ed1
Cesc Nguyen <thuocnv1802@gmail.com>
2021-08-04 23:56:54 +0700

    demo lifecycle

--- example/main.go
+++ example/main.go
@@ -1,1 +1,50 @@
-package example
+package main
+
+import (
+	"fmt"
+	lifecycle "github.com/cesc1802/go-app-lifecycle"
+)
+
+type HelloWorldHttpService struct {
+
+}
+
+func (service *HelloWorldHttpService) Start() error {
+	fmt.Print("start helloworld service")
+	return nil
+}
+
+func (service *HelloWorldHttpService) Stop() error {
+	fmt.Print("stop helloworld service")
+	return nil
+}
+
+type OrderdHttpService struct {
+
+}
+
+func (service *OrderdHttpService) Start() error {
+	fmt.Print("start order service")
+	return nil
+}
+
+func (service *OrderdHttpService) Stop() error {
+	fmt.Print("stop order service")
+	return nil
+}
+
+func main() {
+	var service HelloWorldHttpService
+	var orderService OrderdHttpService
+
+	app := lifecycle.New(lifecycle.Services(&service, &orderService), lifecycle.Name("hello service"))
+
+	if err := app.Run(); err != nil {
+		fmt.Printf("Finish with error: %+v\n", err)
+		return
+	}
+	fmt.Printf("Done!")
+}
+
+
+
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cesc1802/go-app-lifecycle
 
 go 1.16
+
+require golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
